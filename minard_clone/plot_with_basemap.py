99bfc3b2c3b36d7f6a17e345198774fef28cc687
mike <mikeyang1015@gmail.com>
2026-03-17 15:15:16 +0800

    完成氣溫折線圖的繪製

--- minard_clone/plot_with_basemap.py
+++ minard_clone/plot_with_basemap.py
@@ -1,19 +1,23 @@
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
-import pymysql
+# import pymysql
 import pandas as pd
+from sqlalchemy import create_engine
 
-connection = pymysql.connect(
+engine = create_engine(
+    "mysql+pymysql://root:password@localhost:3306/minard"
+)
+"""connection = pymysql.connect(
     host = "localhost",
     port = 3306,
     user = "root",
     password = "password",
     database = "minard",
     charset = "utf8mb4"
-    )
-city_df = pd.read_sql("""select * from cities;""", con = connection)
-temp_df = pd.read_sql("""select * from temperatures;""", con = connection)
-army_df = pd.read_sql("""select * from army;""", con = connection)
+    )"""
+city_df = pd.read_sql("""select * from cities;""", con = engine)
+temp_df = pd.read_sql("""select * from temperatures;""", con = engine)
+army_df = pd.read_sql("""select * from army;""", con = engine)
 
 lonc = city_df["lonc"].values
 latc = city_df["latc"].values
@@ -23,7 +27,9 @@ lonps = army_df["lonp"].values
 latps = army_df["latp"].values
 direction = army_df["direc"].values
 survivals = army_df["surviv"].values
-
+temp_celsius = (temp_df["temp"] * 1.25).astype(int)
+lonts = temp_df["lont"].values
+annotations = temp_celsius.astype(str).str.cat(temp_df["date"], sep = "°C ")
 fig, axes = plt.subplots(nrows = 2, figsize = (25, 12), gridspec_kw = {"height_ratios" : [4, 1]})
 m = Basemap(projection = "lcc", resolution = "i", width = 1000000, height = 400000,
             lon_0 = 31, lat_0 = 55, ax = axes[0])
@@ -44,21 +50,18 @@ for i in range(rows - 1):
     start_stop_lats = (y[i], y[i + 1])
     line_width = survivals[i]
     m.plot(start_stop_lons, start_stop_lats, linewidth = line_width/10000, color = line_color, zorder = 1)
-plt.show()
 
-
-"""temp_celsius = (temp_df["temp"] * 1.25).values
-lons = temp_df["lont"].values
-fig, ax = plt.subplots()
-ax.plot(lons, temp_celsius)
-plt.show()"""
-
-
-"""rows = army_df.shape[0]
-lons = army_df["lonp"].values
-lats = army_df["latp"].values
-survivals = army_df["surviv"].values
-direction = army_df["direc"].values
-fig, ax = plt.subplots()
-
-plt.show()"""
+axes[1].plot(lonts, temp_celsius, linestyle = "dashed", color = "black")
+for lont, temp_c, annotation in zip(lonts, temp_celsius, annotations):
+    axes[1].annotate(annotation, xy = (lont - 0.3, temp_c - 7), fontsize = 16)
+axes[1].set_ylim(-50, 10)
+axes[1].spines["top"].set_visible(False)
+axes[1].spines["right"].set_visible(False)
+axes[1].spines["left"].set_visible(False)
+axes[1].spines["bottom"].set_visible(False)
+axes[1].grid(True, which = "major", axis = "both")
+axes[1].set_xticklabels([])
+axes[1].set_yticklabels([])
+axes[0].set_title("Napolean's disastrous Russian campaign of 1812", loc = "left", fontsize = 30)
+plt.tight_layout()
+fig.savefig("minard_clone.png")
